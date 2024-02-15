9df2ebaca63d114ef09813e33efe4e98055c9bd4
Agustina Balcarcel <balcarcelagustina@gmail.com>
2024-02-15 16:10:29 -0300

    Initial commit Create models

---
+++ Backend_SDH/Models/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace Backend_SDH.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CookMethodId { get; set; }
+        public CookMethod CookMethod { get; set; }
+        public List<RecipeIngredient> RecipeIngredients { get; set; }
+        public decimal KcalTotal { get; set; }
+    }
+}
---
+++ Backend_SDH/Models/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend_SDH.Models
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
