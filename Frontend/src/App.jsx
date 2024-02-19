50cb725f4549447418ee72698c430df3016b2f2b
Matias Campos <lolo252000@gmail.com>
2024-02-19 12:24:35 -0300

    Style: completando estilos y mediaquery

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,8 +3,9 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="w-full">
+    <div className="w-10/12 lg:w-full mx-auto lg:flex lg:flex-row rounded-t-lg">
       <IngredientFilter />
+      <h1 className="lg:w-2/3">ffffffffffffffffffffffffffffffffffff</h1>
     </div>
   );
 }
--- Frontend/src/components/IngredientFilter.jsx
+++ Frontend/src/components/IngredientFilter.jsx
@@ -4,22 +4,39 @@ import { useState } from "react";
 
 const IngredientFilter = () => {
 
-    const [ingredients, setIngredients] = useState([1, 2]);
+    const [ingredients, setIngredients] = useState([]);
+
+    function handleEnterPress(e) {
+        if (e.key === 'Enter' && e.target.value.trim() !== "") {
+            setIngredients([...ingredients, e.target.value]);
+            e.target.value = "";
+        }
+    }
+
+    function handleDeleteArray() {
+        setIngredients([]);
+    }
+
+    function handleDeleteIngredient(e) {
+        const ingredientToDelete = e.target.parentElement.textContent.trim();
+        setIngredients(ingredients.filter(ingredient => ingredient !== ingredientToDelete));
+    }
+
     return (
-        <div className="w-10/12 m-auto relative my-6">
-            <div className="flex items-center gap-x-5 mb-3">
+        <div className="relative py-6 lg:w-1/3 px-8 lg:bg-gray-100 lg:h-screen">
+            <div className="flex lg:flex-col items-center gap-x-5 gap-y-5 mb-5">
                 <h2 className="flex-shrink-0">Agrega tus ingredientes para empezar</h2>
-                <div className="flex w-full bg-gray-100 px-2 rounded-lg">
+                <div className="flex w-full bg-gray-100 lg:bg-white px-2 rounded-lg">
                     <FaSearch className="self-center" />
-                    <input className="flex-grow bg-gray-100 px-2" type="text" placeholder="Frutas, verduras, lacteos, proteinas..." />
+                    <input className="flex-grow bg-gray-100 lg:bg-white px-2" type="text" placeholder="Frutas, verduras, lacteos, proteinas..." onKeyPress={handleEnterPress} />
                 </div>
             </div>
-            <ul className="flex gap-x-5 mb-3">
+            <ul className="flex flex-wrap gap-x-3 gap-y-3 mb-5">
                 {ingredients.map((ingredient, index) => (
-                    <li className="flex bg-gray-100 px-2 rounded-lg gap-x-3" key={index}><RxCross2 className="self-center" />{ingredient}</li>
+                    <li className="flex bg-gray-100 lg:bg-white px-2 rounded-lg gap-x-3" key={index}><RxCross2 className="self-center" onClick={handleDeleteIngredient} />{ingredient}</li>
                 ))}
             </ul>
-            <button className="absolute right-0">Eliminar todos los ingredientes</button>
+            <button className="absolute right-8 lg:right-auto" onClick={handleDeleteArray}>Eliminar todos los ingredientes</button>
         </div>
     )
 }
