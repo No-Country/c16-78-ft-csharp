7a05149fdd60ef7129d9a13c7081214986d061a9
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2024-02-19 17:22:16 -0300

    add primary color with tailwind

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors:{
+      'green':'#00FF94',      
+    }
   },
   plugins: [],
 };
