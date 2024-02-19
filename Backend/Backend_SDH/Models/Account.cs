9f292da482efdaefaf1e414f350505029717aa95
Agustina Balcarcel <balcarcelagustina@gmail.com>
2024-02-19 15:25:58 -0300

    Add BaseEntity class to revert guid/id issues Remove Account class

--- Backend/Backend_SDH/Models/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Backend_SDH.Models
-{
-    public class Account
-    {
-        public Guid Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
---
+++ Backend/Backend_SDH/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend_SDH.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+    }
+}
