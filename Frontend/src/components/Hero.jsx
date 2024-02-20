d55ce4868e660ee53b948e569f9a4920aafe89fe
Nahuel Benitez <jorgenahuelbenitez07@gmail.com>
2024-02-19 21:31:26 -0300

    download images and add in cards in hero

--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -1,56 +1,60 @@
 import React from "react";
+import taco from '../assets/taco.png'
+import cereza from '../assets/cereza.png'
+import frutilla from '../assets/fresa.png'
+import uva from '../assets/uva.png'
+import sushi from '../assets/sushi.png'
+import pasta from '../assets/pasta.png'
 
 const HeroSection = () => {
   return (
-    <section className="bg-gray-100 p-8">
+    <section className="bg-green pb-14 py-2">
       <div className="max-w-screen-xl mx-auto flex flex-wrap items-center">
         <div className="w-full md:w-1/2 mb-8 md:mb-0">
-          <h1 className="text-3xl md:text-4xl font-bold text-navy mb-4">Crea platillos con los ingredientes de tu nevera</h1>
+          <h3 className="text-3xl md:text-4xl font-normal text-navy mb-4">Crea platillos con los <br></br>ingredientes de tu nevera</h3>
           <p className="text-gray-600">Descubre recetas deliciosas utilizando los ingredientes que ya tienes en tu nevera. ¡Aprovecha al máximo lo que tienes!          
           </p>
-          <a href="#" class="inline-flex items-center justify-center text-2xl text-navy font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
-                Comienza a Cocinar
-            </a>
-
+          <a href="#" className="inline-flex items-center justify-center text-2xl text-navy font-medium text-center rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 ">
+            Comienza a Cocinar
+          </a>
         </div>
         <div className="w-full md:w-1/2 flex justify-center">
-          <div className="grid grid-cols-3 gap-4">
+          <div className="grid grid-cols-2 gap-4">
             {/* Columna 1 */}
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen1.jpg" alt="Card 1" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 1</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={taco} alt="Card 1" className="object-cover  rounded-md mx-auto" />              
             </div>
 
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen2.jpg" alt="Card 2" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 2</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={uva} alt="Card 2" className=" object-cover  rounded-md mx-auto" />
+              
             </div>
 
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen3.jpg" alt="Card 3" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 3</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={cereza} alt="Card 3" className=" object-cover  rounded-md mx-auto" />
+              
             </div>
 
             {/* Columna 2 */}
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen4.jpg" alt="Card 4" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 4</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={sushi} alt="Card 4" className=" object-cover  rounded-md mx-auto" />
+              
             </div>
 
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen5.jpg" alt="Card 5" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 5</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={pasta} alt="Card 5" className="bject-cover  rounded-md mx-auto" />
+              
             </div>
 
-            <div className="bg-white p-4 rounded-md shadow-md">
-              {/* Aquí puedes colocar la imagen y cualquier otro contenido que desees */}
-              <img src="ruta/de/la/imagen6.jpg" alt="Card 6" className="w-full h-32 object-cover mb-4 rounded-md" />
-              <p className="text-gray-800 font-semibold">Card 6</p>
+            <div className="bg-white  rounded-md shadow-md w-32 h-32">
+              
+              <img src={frutilla} alt="Card 6" className="object-cover  rounded-md mx-auto" />
+              
             </div>
           </div>
         </div>
