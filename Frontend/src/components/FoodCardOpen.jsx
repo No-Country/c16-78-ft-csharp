a5a730f1771b8402bc765089ddc956f33d0339d0
JasonMrR <106495420+JasonMrR@users.noreply.github.com>
2024-03-01 17:57:49 -0600

    Modificaciones en las cards correcciones de ingredientes parte1

--- Frontend/src/components/FoodCardOpen.jsx
+++ Frontend/src/components/FoodCardOpen.jsx
@@ -10,25 +10,25 @@ const FoodCardOpen = ({ item, cardOpen, closeCard }) => {
 
   return (
     <section
-      className={`fixed top-0 left-0 xsm:p-4 w-full h-svh overflow-scroll xsm:h-full xsm:justify-center xsm:items-center bg-background-modal ${
+      className={`fixed top-0 left-0 w-full min-h-svh flex lg:justify-center lg:items-center bg-background-modal ${
         cardOpen ? "flex" : "hidden"
       }`}
     >
-      <article className="text-black w-full xsm:w-auto bg-background-card xsm:rounded-3xl overflow-hidden flex flex-col md:flex-row md:w-auto md:max-w-screen-lg">
-        <figure className="relative overflow-hidden flex items-center md:rounded-3xl">
+      <article className="bg-white w-full h-svh lg:h-auto overflow-scroll flex flex-col max-w-screen-xl lg:flex-row lg:m-4 lg:rounded-3xl lg:overflow-hidden">
+        <figure className="relative h-56 xsm:h-64 sm:h-80 md:h-96 lg:h-128 lg:w-128">
           <img
-            className="w-full h-80 md:h-96 md:w-96 object-cover"
+            className="h-full w-full object-cover lg:rounded-3xl"
             src={item.imageSrc}
             alt={item.title}
           />
           <button
-            className="text-xl xsm:text-3xl absolute top-1 xsm:top-4 left-1 xsm:left-4 text-white hover:text-red-500 ease-in-out transition-all"
+            className="text-2xl xsm:text-3xl absolute top-2 xsm:top-4 left-2 xsm:left-4 text-white hover:text-red-500 ease-in-out transition-all"
             onClick={closeCard}
           >
             <TfiClose />
           </button>
           <button
-            className={`text-2xl xsm:text-4xl absolute top-1 xsm:top-4 right-1 xsm:right-4 ${
+            className={`text-3xl xsm:text-4xl absolute top-2 xsm:top-4 right-2 xsm:right-4 ${
               favorite ? "text-red-600" : "text-white"
             }  hover:text-red-400 ease-in-out transition-all`}
             onClick={() => {
@@ -38,7 +38,7 @@ const FoodCardOpen = ({ item, cardOpen, closeCard }) => {
             {favorite ? <MdFavorite /> : <MdFavoriteBorder />}
           </button>
         </figure>
-        <div className="p-4 flex-1 flex flex-col xsm:max-h-full sm:max-h-none">
+        <div className="flex flex-col p-4 md:h-auto flex-1 lg:h-128">
           <header className="flex justify-between lg:items-center">
             <div className="lg:flex justify-center items-center">
               <h3 className="font-semibold text-3xl">{item.title}</h3>
@@ -62,7 +62,7 @@ const FoodCardOpen = ({ item, cardOpen, closeCard }) => {
             </div>
             <time className="text-textHint text-base ml-2">{item.time}</time>
           </header>
-          <div className="flex flex-col">
+          <div className="flex flex-col lg:pb-0 flex-1 justify-start">
             <p className="text-base font-light xsm:line-clamp-2 xl:line-clamp-none">
               {item.description}
             </p>
@@ -74,15 +74,18 @@ const FoodCardOpen = ({ item, cardOpen, closeCard }) => {
               </p>
             </div>
             <h4 className="text-2xl">Ingredientes</h4>
-            <ul className="flex flex-col h-44 xsm:h-32 flex-wrap mb-4">
+            <ul className="flex flex-col h-32 lg:h-20 flex-wrap mb-4">
               {item.ingredients?.map((item, index) => {
                 return (
-                  <li key={index}>
+                  <li key={index} className="ml-1">
                     <p className="italic font-light">- {item}</p>
                   </li>
                 );
               })}
             </ul>
+            <ButtonFill addClass="mt-auto w-full lg:w-fit">
+              Modificar platillo
+            </ButtonFill>
           </div>
         </div>
       </article>
